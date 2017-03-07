2d10b3c4cafe1ef2488b384d9415ffcaa3c0b13e
kantuni <henrikh.kantuni@gmail.com>
2017-03-06 21:24:29 -0500

    Add Week 1 - Problem 2

---
+++ ITMOx - How to Win Coding Competitions/Week 1/a+b_2.cpp
@@ -0,0 +1,17 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+int main() {
+  long long a, b;
+  cin >> a >> b;
+  cout << a + b * b << "\n";
+
+  return 0;
+}
