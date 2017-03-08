1ea398d9dd7a45db7d293617fd483e1782afdbb7
kantuni <henrikh.kantuni@gmail.com>
2017-03-07 21:53:18 -0500

    Add Week 1 - Create a Team!

---
+++ ITMOx - How to Win Coding Competitions/Week 1/create_a_team.cpp
@@ -0,0 +1,45 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+#include <cmath>
+#include <iomanip>
+
+int main() {
+  int t[3][3];
+  
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      cin >> t[i][j];
+    }
+  }
+  
+  double maximum = 0;
+  for (int i = 0; i < 3; ++i) {
+    int a = t[i][0];
+    for (int j = 0; j < 3; ++j) {
+      if (j != i) {
+        int b = t[j][1];
+        for (int k = 0; k < 3; ++k) {
+          if (k != i && k != j) {
+            int c = t[k][2];
+            double e = sqrt(a * a + b * b + c * c);
+            if (e > maximum) {
+              maximum = e;
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  cout << std::setprecision(17) << maximum << '\n';
+  
+  return 0;
+}
