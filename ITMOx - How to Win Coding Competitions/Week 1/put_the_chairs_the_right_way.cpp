0a201399e1201485927106c6b2dab6cebdae527b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-03-08 12:31:55 -0500

    Update put_the_chairs_the_right_way.cpp

--- ITMOx - How to Win Coding Competitions/Week 1/put_the_chairs_the_right_way.cpp
+++ ITMOx - How to Win Coding Competitions/Week 1/put_the_chairs_the_right_way.cpp
@@ -14,6 +14,7 @@ int main() {
   int a, b, c;
   cin >> a >> b >> c;
   
+  // the inner triangle is similar to the outer triangle
   double d = a / 2.0 + b / 2.0 + c / 2.0;
   cout << std::fixed << std::setprecision(8);
   cout << d / 3.0 << '\n';
