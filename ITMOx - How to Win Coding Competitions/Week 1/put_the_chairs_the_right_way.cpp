c11504b915a80844c23f04aa2ff657aff30ebbc6
kantuni <henrikh.kantuni@gmail.com>
2017-03-08 12:29:59 -0500

    Add Week 1 - Problem 5

---
+++ ITMOx - How to Win Coding Competitions/Week 1/put_the_chairs_the_right_way.cpp
@@ -0,0 +1,21 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+#include <iomanip>
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  
+  double d = a / 2.0 + b / 2.0 + c / 2.0;
+  cout << std::fixed << std::setprecision(8);
+  cout << d / 3.0 << '\n';
+  return 0;
+}
