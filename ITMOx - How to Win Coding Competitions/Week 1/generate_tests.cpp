c0bc1e1cce408efdfd43c6ccfa614989e8fc8892
kantuni <henrikh.kantuni@gmail.com>
2017-03-13 22:08:41 -0400

    Generate Tests - Part I

---
+++ ITMOx - How to Win Coding Competitions/Week 1/generate_tests.cpp
@@ -0,0 +1,16 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+int main() {
+  long k;
+  cin >> k;
+
+  return 0;
+}
