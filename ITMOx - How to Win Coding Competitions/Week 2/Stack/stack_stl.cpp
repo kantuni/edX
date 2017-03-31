9ff6453ec67d8fb2e0fe1ee036f03eefc75b3759
kantuni <henrikh.kantuni@gmail.com>
2017-03-31 14:40:12 -0400

    Stack - Accepted

---
+++ ITMOx - How to Win Coding Competitions/Week 2/Stack/stack_stl.cpp
@@ -0,0 +1,46 @@
+#ifdef JUDGE
+  #include <fstream>
+  std::ifstream cin("input.txt");
+  std::ofstream cout("output.txt");
+#else
+  #include <iostream>
+  using std::cin;
+  using std::cout;
+#endif
+
+// std::to_string
+#include <string>
+#include <stack>
+
+typedef long long ll;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  std::string answer = "";
+  
+  std::stack<ll> s;
+  for (int i = 0; i < n; ++i) {
+    char operation;
+    cin >> operation;
+    
+    switch (operation) {
+      case '+': {
+        ll x;
+        cin >> x;
+        s.push(x);
+        break;
+      }
+      case '-': {
+        answer += std::to_string(s.top()) + "\n";
+        s.pop();
+        break;
+      }
+    }
+  }
+  
+  cout << answer;
+  
+  return 0;
+}
