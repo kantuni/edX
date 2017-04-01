3fc27303874a23cd81ad9f2f14b65d64f9a1e37c
kantuni <henrikh.kantuni@gmail.com>
2017-03-31 22:14:28 -0400

    Bracket Sequence - Accepted

--- ITMOx - How to Win Coding Competitions/Week 2/Bracket Sequence/bracket_sequence.cpp
+++ ITMOx - How to Win Coding Competitions/Week 2/Bracket Sequence/bracket_sequence.cpp
@@ -8,6 +8,58 @@
   using std::cout;
 #endif
 
+#include <string>
+#include <stack>
+
 int main() {
+  int n;
+  cin >> n;
+  
+  std::string answer = "";
+  
+  for (int i = 0; i < n; ++i) {
+    std::string line;
+    cin >> line;
+    
+    bool valid = true;
+    std::stack<char> s;
+
+    for (int j = 0; j < line.length(); ++j) {
+      switch (line[j]) {
+        case '(': {
+          s.push('(');
+          break;
+        }
+        case ')': {
+          if (!s.empty() && s.top() == '(') {
+            s.pop();
+          } else {
+            valid = false;
+          }
+          break;
+        }
+        case '[': {
+          s.push('[');
+          break;
+        }
+        case ']': {
+          if (!s.empty() && s.top() == '[') {
+            s.pop();
+          } else {
+            valid = false;
+          }
+          break;
+        }
+      }
+    }
+
+    if (valid && s.empty()) {
+      answer += "YES\n";
+    } else {
+      answer += "NO\n";
+    }   
+  }
+  
+  cout << answer;
   return 0;
 }
