fcdcbf6faa3e2cc42162a68469aabe38eb656421
kantuni <henrikh.kantuni@gmail.com>
2017-04-01 11:15:38 -0400

    Postfix Notation - Accepted

--- ITMOx - How to Win Coding Competitions/Week 2/Postfix Notation/postfix_notation.cpp
+++ ITMOx - How to Win Coding Competitions/Week 2/Postfix Notation/postfix_notation.cpp
@@ -8,6 +8,48 @@
   using std::cout;
 #endif
 
+#include <stack>
+
 int main() {
+  int n;
+  cin >> n;
+  
+  std::stack<int> s;
+  for (int i = 0; i < n; ++i) {
+    char x;
+    cin >> x;
+    
+    switch (x) {
+      case '+': {
+        int a = s.top();
+        s.pop();
+        int b = s.top();
+        s.pop();
+        s.push(a + b);
+        break;
+      }
+      case '-': {
+        int a = s.top();
+        s.pop();
+        int b = s.top();
+        s.pop();
+        s.push(b - a);
+        break;
+      }
+      case '*': {
+        int a = s.top();
+        s.pop();
+        int b = s.top();
+        s.pop();
+        s.push(a * b);
+        break;
+      }
+      default: {
+        s.push(x - '0');
+      }
+    }
+  }
+  
+  cout << s.top() << "\n";
   return 0;
 }
