66af77ca12655c35a6d558480455d200e99bb381
kantuni <henrikh.kantuni@gmail.com>
2017-04-01 22:02:26 -0400

    Kenobi's Lightsabers - Time limit exceeded

--- ITMOx - How to Win Coding Competitions/Week 2/Kenobis Lightsabers/kenobis_lightsabers.cpp
+++ ITMOx - How to Win Coding Competitions/Week 2/Kenobis Lightsabers/kenobis_lightsabers.cpp
@@ -16,11 +16,42 @@ int main() {
   cin >> n;
   
   std::deque<int> q;
+  int mum = 0;
+  
   for (int i = 0; i < n; ++i) {
     std::string cmd;
     cin >> cmd;
     
+    if (cmd == "mum!") {
+      ++mum;
+      if (i != n - 1) {
+        continue;
+      }
+    }
+    
+    // process mum! commands in batch
+    if (mum > 0) {
+      if (q.size() % 2 == 0) {
+        if (mum % 2 == 1) {
+          for (int j = 0; j < q.size() / 2; ++j) {
+            q.push_back(q[0]);
+            q.pop_front();
+          }
+        }
+      } else {
+        int end = mum * (q.size() / 2) % q.size();
+        for (int j = 0; j < end; ++j) {
+          q.push_back(q[0]);
+          q.pop_front();
+        }
+      }
+      
+      // reset mum
+      mum = 0;
+    }
+    
     if (cmd == "add") {
+      // add the lightsaber
       int x;
       cin >> x;
       q.push_back(x);
@@ -28,18 +59,12 @@ int main() {
     }
     
     if (cmd == "take") {
+      // take the lightsaber
       q.pop_back();
-      continue;
-    }
-    
-    if (cmd == "mum!") {
-      for (int j = 0; j < q.size() / 2; ++j) {
-        q.push_back(q[0]);
-        q.pop_front();
-      }
     }
   }
   
+  // print lightsabers
   cout << q.size() << "\n";
   for (int i = 0; i < q.size(); ++i) {
     cout << q[i] << " ";
