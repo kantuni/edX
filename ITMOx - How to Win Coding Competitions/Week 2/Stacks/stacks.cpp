1648bc7de85cb81a7b65be03a77fabb64d4107eb
kantuni <henrikh.kantuni@gmail.com>
2017-04-01 20:37:16 -0400

    Stacks - Accepted

--- ITMOx - How to Win Coding Competitions/Week 2/Stacks/stacks.cpp
+++ ITMOx - How to Win Coding Competitions/Week 2/Stacks/stacks.cpp
@@ -8,6 +8,39 @@
   using std::cout;
 #endif
 
+// std::greater
+#include <functional>
+#include <vector>
+#include <queue>
+
 int main() {
+  int n;
+  cin >> n;
+  
+  // min heap init
+  std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
+  int max = 1;
+  
+  for (int i = 0; i < n; ++i) {
+    int a;
+    cin >> a;
+    
+    if (i == 0 || a != 0) {
+      // add another stack
+      min_heap.push(1);
+    } else {
+      // find the min stack
+      int min = min_heap.top();
+      min_heap.pop();
+      ++min;
+      min_heap.push(min);
+      // number of cups in the biggest stack
+      if (min > max) {
+        max = min;
+      }
+    }
+  }
+  
+  cout << max << "\n";
   return 0;
 }
