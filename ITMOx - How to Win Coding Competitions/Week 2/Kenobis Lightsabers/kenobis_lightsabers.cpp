3a7a0d4a5b947b98638b6606af6f0c09ec291ed5
kantuni <henrikh.kantuni@gmail.com>
2017-04-01 21:15:13 -0400

    Kenobi's Lightsabers

--- ITMOx - How to Win Coding Competitions/Week 2/Kenobis Lightsabers/kenobis_lightsabers.cpp
+++ ITMOx - How to Win Coding Competitions/Week 2/Kenobis Lightsabers/kenobis_lightsabers.cpp
@@ -8,6 +8,43 @@
   using std::cout;
 #endif
 
+#include <string>
+#include <deque>
+
 int main() {
+  int n;
+  cin >> n;
+  
+  std::deque<int> q;
+  for (int i = 0; i < n; ++i) {
+    std::string cmd;
+    cin >> cmd;
+    
+    if (cmd == "add") {
+      int x;
+      cin >> x;
+      q.push_back(x);
+      continue;
+    }
+    
+    if (cmd == "take") {
+      q.pop_back();
+      continue;
+    }
+    
+    if (cmd == "mum!") {
+      for (int j = 0; j < q.size() / 2; ++j) {
+        q.push_back(q[0]);
+        q.pop_front();
+      }
+    }
+  }
+  
+  cout << q.size() << "\n";
+  for (int i = 0; i < q.size(); ++i) {
+    cout << q[i] << " ";
+  }
+  cout << "\n";
+  
   return 0;
 }
