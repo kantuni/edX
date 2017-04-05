795757cd275e0b479dfef893aa2797cd987fffed
kantuni <henrikh.kantuni@gmail.com>
2017-04-05 19:07:55 -0400

    K-th Ordered Statistic - Input

--- ITMOx - How to Win Coding Competitions/Week 3/kth_ordered_statistic.cpp
+++ ITMOx - How to Win Coding Competitions/Week 3/kth_ordered_statistic.cpp
@@ -8,6 +8,24 @@
   using std::cout;
 #endif
 
+typedef long long ll;
+
 int main() {
+  ll n, k1, k2;
+  cin >> n >> k1 >> k2;
+  
+  ll *a = new ll[n];
+  ll A, B, C;
+  cin >> A >> B >> C >> a[0] >> a[1];
+  
+  for (int i = 2; i < n; ++i) {
+    a[i] = A * a[i - 2] + B * a[i - 1] + C;
+  }
+  
+  for (int i = 0; i < n; ++i) {
+    cout << a[i] << " ";
+  }
+  cout << "\n";
+  
   return 0;
 }
