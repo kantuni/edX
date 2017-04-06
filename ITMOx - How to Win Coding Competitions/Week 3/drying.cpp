cd3679c5579f0ebab481d5b41e102a94da504e07
kantuni <henrikh.kantuni@gmail.com>
2017-04-06 00:55:35 -0400

    Drying - Wrong answer

--- ITMOx - How to Win Coding Competitions/Week 3/drying.cpp
+++ ITMOx - How to Win Coding Competitions/Week 3/drying.cpp
@@ -8,19 +8,66 @@
   using std::cout;
 #endif
 
+#include <list>
+#include <iterator>
+#include <algorithm>
+
 typedef long long ll;
 
 int main() {
   int n;
   cin >> n;
   
-  ll *a = new ll[n];
+  std::list<ll> a;
   for (int i = 0; i < n; ++i) {
-    cin >> a[i];
+    ll tmp;
+    cin >> tmp;
+    a.push_back(tmp);
   }
   
   ll k;
   cin >> k;
   
+  a.sort();
+  
+  ll minutes = 0;
+  while (a.back() != 0) {
+    // radiator
+    if (a.back() >= k) {
+      a.back() -= k;
+    } else {
+      a.back() = 0;
+    }
+    
+    std::list<ll>::iterator j = begin(a);
+    std::list<ll>::iterator last = std::prev(end(a));
+    
+    // every minute -1
+    for (auto it = begin(a); it != last; ++it) {
+      if (*it >= 1) {
+        --(*it);
+      }
+
+      if (*it <= *last) {
+        j = it;
+      }
+    }
+    
+    // insert the previous max into the corresponding position
+    if (a.size() > 1) {
+      ll tmp = a.back();
+      a.pop_back();
+      a.insert(j, tmp);
+    }
+    
+    for (auto item: a) {
+      cout << item << " ";
+    }
+    cout << "\n";
+    
+    ++minutes;
+  }
+  
+  cout << minutes << "\n";
   return 0;
 }
