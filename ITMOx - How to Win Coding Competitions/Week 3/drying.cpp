f3452127caf5ea740a4a2d01fcd6d169d066bead
kantuni <henrikh.kantuni@gmail.com>
2017-04-07 23:08:53 -0400

    Drying - Wrong answer

--- ITMOx - How to Win Coding Competitions/Week 3/drying.cpp
+++ ITMOx - How to Win Coding Competitions/Week 3/drying.cpp
@@ -8,21 +8,29 @@
   using std::cout;
 #endif
 
-#include <vector>
-#include <queue>
 #include <cmath>
+#include <queue>
 
 typedef long long ll;
+
+struct Dress {
+  ll water;
+  ll skip = 0;
+  
+  bool operator <(const Dress &other) const {
+    return water < other.water;
+  }
+};
 
 int main() {
   int n;
   cin >> n;
   
   // max heap init
-  std::priority_queue<int> a;
+  std::priority_queue<Dress> a;
   for (int i = 0; i < n; ++i) {
-    ll tmp;
-    cin >> tmp;
+    Dress tmp;
+    cin >> tmp.water;
     a.push(tmp);
   }
   
@@ -31,54 +39,60 @@ int main() {
   
   ll minutes = 0;
   
-  if (a.size() == 1 && a.top() > k) {
-    minutes = ceil((double) a.top() / k);
+  if (a.size() == 1) {
+    minutes = ceil((double) a.top().water / k);
   } else {
-    ll max1 = a.top();
+    Dress max1 = a.top();
     a.pop();
-    ll max2 = a.top();
+    Dress max2 = a.top();
     
-    while (max1 != 0 || max2 != 0) {
-      // m = how many minutes max1 was the max
-      // max1 - k * x >= max2 - 1 * x
-      ll m = ceil((double) (max1 - max2) / (k - 1));
+    while (max1.water != 0 || max2.water != 0) {
+      cout << max1.water << " " << max2.water << "\n";
       
-      // at least 1 minute max1 was the max
-      if (max1 == max2) {
+      // m = how many minutes max1 was on the radiator
+      // max1 - k * x >= max2 - 1 * x
+      ll m = ceil((double)(max1.water - max2.water) / (k - 1));
+      
+      // at least 1 minute max1 was on the radiator
+      if (max1.water == max2.water) {
         m = 1;
       }
       
       minutes += m;
       
       // update max1 value, and put it back
-      if (max1 - m * k > 0) {
-        max1 -= m * k;
+      if (max1.water - m * k > 0) {
+        max1.water -= m * k;
       } else {
-        max1 = 0;
+        max1.water = 0;
       }
+      
+      max1.skip = minutes;
       a.push(max1);
       
       // new max1
       max1 = a.top();
-      if (max1 > minutes) {
-        max1 -= minutes;
+      if (max1.water > minutes - max1.skip) {
+        max1.water -= (minutes - max1.skip);
+        max1.skip = minutes;
       } else {
-        max1 = 0;
+        max1.water = 0;
+        max1.skip = minutes;
       }
       a.pop();
       
       // new max 2
       max2 = a.top();
-      if (max1 > minutes) {
-        max2 -= minutes;
+      if (max2.water > minutes - max2.skip) {
+        max2.water -= (minutes - max2.skip);
       } else {
-        max2 = 0;
+        max2.water = 0;
       }
     }
     
-    // print
+    cout << "\nQueue:\n";
     while (!a.empty()) {
-      cout << a.top() << " ";
+      printf("(%lld, %lld) ", a.top().water, a.top().skip);
       a.pop();
     }
     cout << "\n";
