805404675595e7b75ce0908fe5af9e41881cd317
kantuni <henrikh.kantuni@gmail.com>
2017-04-14 18:05:55 -0400

    K Best - Wrong answer on Test 8

--- ITMOx - How to Win Coding Competitions/Week 3/k_best.cpp
+++ ITMOx - How to Win Coding Competitions/Week 3/k_best.cpp
@@ -8,7 +8,42 @@
   using std::cout;
 #endif
 
+#include <iterator>
+#include <vector>
+#include <algorithm>
+
+typedef unsigned long long ull;
+
+struct Jewel {
+  int index;
+  ull v;
+  ull w;
+  
+  friend bool operator<(const Jewel &a, const Jewel &b) {
+    return (a.v * 1.0) / a.w > (b.v * 1.0) / b.w;
+  }
+};
+
 int main() {
+  int n, k;
+  cin >> n >> k;
+  
+  std::vector<Jewel> jewels(n);
+  
+  for (int i = 0; i < n; ++i) {
+    Jewel j;
+    cin >> j.v >> j.w;
+    j.index = i;
+    jewels[i] = j;
+  }
+  
+  std::sort(begin(jewels), end(jewels));
+  
+  for (int i = 0; i < k; ++i) {
+    cout << jewels[i].index + 1 << " ";
+  }
+  cout << "\n";
+  
   return 0;
 }
 
