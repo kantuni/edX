24fc750604fafcab7e0e4dbeaff3554f19140b3a
kantuni <henrikh.kantuni@gmail.com>
2017-04-15 13:30:09 -0400

    Still WA on Test 14

--- ITMOx - How to Win Coding Competitions/Week 3/K Best/k_best.cpp
+++ ITMOx - How to Win Coding Competitions/Week 3/K Best/k_best.cpp
@@ -11,6 +11,7 @@
 #include <iterator>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 typedef unsigned long long ull;
 
@@ -32,31 +33,30 @@ int main() {
   ull W = 0;
   
   std::priority_queue<Jewel, std::vector<Jewel>> best;
-  for (int i = 0; i < k; ++i) {
+  std::vector<Jewel> jewels(n);
+  for (int i = 0; i < n; ++i) {
     Jewel j;
     cin >> j.v >> j.w;
     j.index = i + 1;
-    V += j.v;
-    W += j.w;
-    best.push(j);
+    jewels[i] = j;
   }
   
-  std::vector<Jewel> rest(n);
-  for (int i = 0; i < n - k; ++i) {
-    Jewel j;
-    cin >> j.v >> j.w;
-    j.index = k + i + 1;
-    rest[i] = j;
+  std::sort(begin(jewels), end(jewels));
+  
+  for (int i = 0; i < k; ++i) {
+    V += jewels[i].v;
+    W += jewels[i].w;
+    best.push(jewels[i]);
   }
   
-  for (int i = 0; i < n - k; ++i) {
+  for (int i = k; i < n; ++i) {
     Jewel t = best.top();
-    ull newV = V - t.v + rest[i].v;
-    ull newW = W - t.w + rest[i].w;
+    ull newV = V - t.v + jewels[i].v;
+    ull newW = W - t.w + jewels[i].w;
     
     if ((double) newV / newW > (double) V / W) {
       best.pop();
-      best.push(rest[i]);
+      best.push(jewels[i]);
       V = newV;
       W = newW;
     }
