a00195461ed7e252bbd02f23406745e3657348f3
kantuni <henrikh.kantuni@gmail.com>
2017-04-15 23:32:48 -0400

    Scarecrow Sort - WA on Test 14

--- ITMOx - How to Win Coding Competitions/Week 3/Scarecrow Sort/scarecrow_sort.cpp
+++ ITMOx - How to Win Coding Competitions/Week 3/Scarecrow Sort/scarecrow_sort.cpp
@@ -8,7 +8,48 @@
   using std::cout;
 #endif
 
+typedef long long ll;
+
 int main() {
+  int n, k;
+  cin >> n >> k;
+  
+  ll *m = new ll[n];
+  for (int i = 0; i < n; ++i) {
+    cin >> m[i];
+  }
+  
+  for (int i = 0; i < n - k; ++i) {
+    if (m[i] > m[i + k]) {
+      ll tmp = m[i];
+      m[i] = m[i + k];
+      m[i + k] = tmp;
+      
+      for (int j = i; j > 0; --j) {
+        if (m[j] < m[j - 1]) {
+          ll tmp = m[j];
+          m[j] = m[j - 1];
+          m[j - 1] = tmp;
+        }
+      }
+    }
+  }
+  
+  bool sorted = true;
+  for (int i = 0; i < n - 1; ++i) {
+    if (m[i] > m[i + 1]) {
+      sorted = false;
+      break;
+    }
+  }
+  
+  if (sorted) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+  
+  delete[] m;
   return 0;
 }
 
