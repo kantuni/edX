9cabd592aa2b58d48be700998a72b02bf4d69cd7
kantuni <henrikh.kantuni@gmail.com>
2017-04-18 23:30:32 -0400

    Find a Cycle - Almost there

--- ITMOx - How to Win Coding Competitions/Week 4/Find a Cycle/find_a_cycle.cpp
+++ ITMOx - How to Win Coding Competitions/Week 4/Find a Cycle/find_a_cycle.cpp
@@ -9,36 +9,107 @@
 #endif
 
 #include <vector>
+#include <stack>
 
 int main() {
   int n, m;
   cin >> n >> m;
   
   std::vector<std::vector<int>> graph(n);
-  bool loop = false;
+  // 0: white, 1: grey, 2: black
+  std::vector<int> color(n, 0);
+  
+  std::stack<int> v;
+  int start = -1;
+  bool cycle = false;
+  
   for (int i = 0; i < m; ++i) {
     int s, t;
     cin >> s >> t;
     
     if (s == t) {
-      loop = true;
+      // loop
+      cycle = true;
+      v.push(s - 1);
+      v.push(t - 1);
+    }
+    
+    if (start == -1) {
+      start = s - 1;
     }
     
     graph[s - 1].push_back(t - 1);
-    graph[t - 1].push_back(s - 1);
+  }
+ 
+  if (!cycle) {
+    v.push(start);
+    color[start] = 1;
   }
   
-  if (loop) {
-    cout << "YES\n";
-    return 0;
-  }
-  
-  for (int i = 0; i < graph.size(); ++i) {
-    cout << i + 1 << ": ";
-    for (int j = 0; j < graph[i].size(); ++j) {
-      cout << graph[i][j] + 1 << " ";
+  while (!v.empty() && !cycle) {
+    int next;
+    bool has_next = false;
+    
+    for (int i = 0; i < graph[v.top()].size(); ++i) {
+      next = graph[v.top()][i];
+      
+      if (color[next] == 1) {
+        // make sure it's not the parent
+        int top = v.top();
+        v.pop();
+        int parent = v.top();
+        v.push(top);
+        
+        if (next != parent) {
+          cycle = true;
+          break;
+        }
+      }
+      
+      if (color[next] == 0) {
+        has_next = true;
+        break;
+      }
+    }
+
+    if (has_next || cycle) {
+      // grey
+      v.push(next);
+      color[next] = 1;
+    } else {
+      // black
+      color[v.top()] = 2;
+      v.pop();
+    }
+    
+    cout << "\n";
+    std::stack<int> copy = v;
+    while (!copy.empty()) {
+      cout << copy.top() + 1 << " ";
+      copy.pop();
     }
     cout << "\n";
+  }
+  
+  if (cycle) {
+    cout << "YES\n";
+    
+    std::stack<int> c;
+    int source = v.top();
+    v.pop();
+    while (v.top() != source) {
+      c.push(v.top());
+      v.pop();
+    }
+    
+    c.push(source);
+    while (!c.empty()) {
+      cout << c.top() + 1 << " ";
+      c.pop();
+    }
+    cout << "\n";
+  } else {
+    cout << "NO\n";
   }
   
   return 0;  
