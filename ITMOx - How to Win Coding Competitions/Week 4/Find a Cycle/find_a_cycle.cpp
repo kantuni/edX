bf7a7a0434a4029aa7d508ecf4a745c619482cb6
kantuni <henrikh.kantuni@gmail.com>
2017-04-18 20:19:52 -0400

    Find a Cycle - Input

--- ITMOx - How to Win Coding Competitions/Week 4/Find a Cycle/find_a_cycle.cpp
+++ ITMOx - How to Win Coding Competitions/Week 4/Find a Cycle/find_a_cycle.cpp
@@ -8,7 +8,39 @@
   using std::cout;
 #endif
 
+#include <vector>
+
 int main() {
+  int n, m;
+  cin >> n >> m;
+  
+  std::vector<std::vector<int>> graph(n);
+  bool loop = false;
+  for (int i = 0; i < m; ++i) {
+    int s, t;
+    cin >> s >> t;
+    
+    if (s == t) {
+      loop = true;
+    }
+    
+    graph[s - 1].push_back(t - 1);
+    graph[t - 1].push_back(s - 1);
+  }
+  
+  if (loop) {
+    cout << "YES\n";
+    return 0;
+  }
+  
+  for (int i = 0; i < graph.size(); ++i) {
+    cout << i + 1 << ": ";
+    for (int j = 0; j < graph[i].size(); ++j) {
+      cout << graph[i][j] + 1 << " ";
+    }
+    cout << "\n";
+  }
+  
   return 0;  
 }
 
