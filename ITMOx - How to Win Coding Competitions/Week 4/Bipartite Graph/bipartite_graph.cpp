0bb9f51bd1682389f28d2c81b1f4c2aa989beb9f
kantuni <henrikh.kantuni@gmail.com>
2017-04-19 23:19:25 -0400

    Bipartite Graph - Accepted?

--- ITMOx - How to Win Coding Competitions/Week 4/Bipartite Graph/bipartite_graph.cpp
+++ ITMOx - How to Win Coding Competitions/Week 4/Bipartite Graph/bipartite_graph.cpp
@@ -8,7 +8,75 @@
   using std::cout;
 #endif
 
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+typedef std::vector<int> vi;
+typedef std::vector<std::vector<int>> vii;
+
+bool odd_cycle = false;
+vi color;
+
+void dfs(vii G, int index, int parent) {
+  color[index] = 1;
+  for (int i = 0; i < G[index].size(); ++i) {
+    int adj = G[index][i];
+    if (adj != parent) {
+      if (color[adj] == 0) {
+        dfs(G, adj, index);
+      } else if (color[adj] == 1) {
+        return;
+      }
+    }
+  }
+  color[index] = 2;
+}
+
 int main() {
+  int n, m;
+  cin >> n >> m;
+  
+  vii G(n);
+  color.resize(n, 0);
+  int start = -1;
+  
+  for (int i = 0; i < m; ++i) {
+    int s, t;
+    cin >> s >> t;
+    
+    if (start == -1) {
+      start = s - 1;
+    }
+    
+    if (s == t) {
+      odd_cycle = true;
+    }
+    
+    G[s - 1].push_back(t - 1);
+    G[t - 1].push_back(s - 1);
+  }
+  
+  if (odd_cycle) {
+    cout << "NO\n";
+    return 0;
+  }
+
+  dfs(G, start, -1);
+
+  int ones = 0;
+  for (int i = 0; i < color.size(); ++i) {
+    if (color[i] == 1) {
+      ones += color[i];
+    }
+  }
+  
+  if (ones % 2 == 0) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+  
   return 0;
 }
 
