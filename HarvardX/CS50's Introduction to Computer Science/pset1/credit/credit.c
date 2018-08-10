d6c6111703ad6e01a11b68ffd8fe7fcb64dc86a3
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-10 14:38:27 +0400

    Update credit.c

--- HarvardX/CS50's Introduction to Computer Science/pset1/credit/credit.c
+++ HarvardX/CS50's Introduction to Computer Science/pset1/credit/credit.c
@@ -4,20 +4,20 @@
 
 int main()
 {
-    // prompt user for input
+    // Prompt user for a number.
     long long n = get_long_long("Number: ");
-    // get length of the number
+    // Calculate the length of the number.
     int m = (n == 0) ? 1 : floor(log10(n)) + 1;
-    // create array for digits
+    // Put digits of the number in an array.
     int d[m];
     for (int i = 0; i < m; i++)
     {
         d[m - i - 1] = n % 10;
         n /= 10;
     }
-    // multiply every other digit by 2,
+    // Multiply every other digit by 2,
     // starting with the number’s second-to-last digit,
-    // and then add those products' digits together
+    // and then add those products' digits together.
     int sum = 0;
     for (int i = m - 2; i >= 0; i -= 2)
     {
@@ -28,28 +28,23 @@ int main()
             tmp /= 10;
         }
     }
-    // add the sum to the sum of the digits
-    // that weren’t multiplied by 2
+    // Add the sum to the sum of the digits
+    // that weren’t multiplied by 2.
     for (int i = m - 1; i >= 0; i -= 2)
     {
         sum += d[i];
     }
-    // if the total’s last digit is 0
-    // the number is valid
-    if (sum % 10 != 0)
+    // If the total’s last digit is 0, the number is valid.
+    if (sum % 10 == 0)
     {
-        printf("INVALID\n");
-    }
-    else
-    {
-        // American Express uses 15-digit numbers
-        // American Express numbers all start with 34 or 37
+        // American Express uses 15-digit numbers.
+        // American Express numbers all start with 34 or 37.
         bool amex = m == 15 && d[0] == 3 && (d[1] == 4 || d[1] == 7);
-        // MasterCard uses 16-digit numbers
-        // MasterCard numbers all start with 51, 52, 53, 54, or 55
+        // MasterCard uses 16-digit numbers.
+        // MasterCard numbers all start with 51, 52, 53, 54, or 55.
         bool mastercard = m == 16 && d[0] == 5 && d[1] > 0 && d[1] < 6;
-        // Visa uses 13- and 16-digit numbers
-        // Visa numbers all start with 4
+        // Visa uses 13- and 16-digit numbers.
+        // Visa numbers all start with 4.
         bool visa = (m == 13 || m == 16) && d[0] == 4;
         if (amex)
         {
@@ -67,6 +62,10 @@ int main()
         {
             printf("INVALID\n");
         }
+    }
+    else
+    {
+        printf("INVALID\n");
     }
     return 0;
 }
