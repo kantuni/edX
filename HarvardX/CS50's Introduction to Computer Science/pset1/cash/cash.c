dd77ccf85559a575a376df493110634512d08a11
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-10 14:34:24 +0400

    Update cash.c

--- HarvardX/CS50's Introduction to Computer Science/pset1/cash/cash.c
+++ HarvardX/CS50's Introduction to Computer Science/pset1/cash/cash.c
@@ -3,27 +3,28 @@
 
 int main()
 {
-    // prompt user for an amount of change
+    // Prompt user for an amount of change.
+    // The amount should not be negative.
     float n;
     do
     {
         n = get_float("Change owed: ");
     }
     while (n < 0);
-    // convert dollars to cents
+    // Convert dollars into cents.
     int cents = n * 100 + 0.5;
-    // calculate the minimum number of coins
+    // Calculate the minimum number of coins.
     int ans = 0;
-    // quarters
+    // Calculate the number of quarters used.
     ans += cents / 25;
     cents %= 25;
-    // dimes
+    // Calculate the number of dimes used.
     ans += cents / 10;
     cents %= 10;
-    // nickles
+    // Calculate the number of nickles used.
     ans += cents / 5;
     cents %= 5;
-    // pennies
+    // Calculate the number of pennies used.
     ans += cents;
     printf("%i\n", ans);
     return 0;
