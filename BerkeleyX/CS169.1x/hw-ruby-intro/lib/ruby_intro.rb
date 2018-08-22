3f794736dfc642a357cd1866905767b4ac406914
kantuni <henrikh.kantuni@gmail.com>
2018-08-22 20:40:33 +0400

    Ruby Intro - Part 3

--- BerkeleyX/CS169.1x/hw-ruby-intro/lib/ruby_intro.rb
+++ BerkeleyX/CS169.1x/hw-ruby-intro/lib/ruby_intro.rb
@@ -34,6 +34,7 @@ def sum_to_n?(array, n)
   !match.nil?
 end
 
+
 # Part 2
 
 # Define a method hello(name) that takes a string representing a name and
@@ -61,8 +62,31 @@ def binary_multiple_of_4?(s)
   (s.to_i(2) % 4).zero?
 end
 
+
 # Part 3
 
+# Define a class BookInStock which represents a book with an ISBN number, isbn,
+# and price of the book as a floating-point number, price, as attributes.
+# The constructor should accept the ISBN number (a string, since in real life
+# ISBN numbers can begin with zero and can include hyphens) as the first
+# argument and price as second argument, and should raise ArgumentError
+# (one of Ruby's built-in exception types) if the ISBN number is
+# the empty string or if the price is less than or equal to zero.
+# Include the proper getters and setters for these attributes.
+# Include a method price_as_string that returns the price of the book formatted
+# with a leading dollar sign and two decimal places, that is, a price of 20
+# should format as "$20.00" and a price of 33.8 should format as "$33.80".
 class BookInStock
-  # YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$#{format('%.2f', @price)}"
+  end
 end
