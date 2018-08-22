3d0d2819c6880be05b0da398b6a9261730d042f1
kantuni <henrikh.kantuni@gmail.com>
2018-08-22 19:58:04 +0400

    Ruby Intro - Part 2

--- BerkeleyX/CS169.1x/hw-ruby-intro/lib/ruby_intro.rb
+++ BerkeleyX/CS169.1x/hw-ruby-intro/lib/ruby_intro.rb
@@ -36,16 +36,29 @@ end
 
 # Part 2
 
+# Define a method hello(name) that takes a string representing a name and
+# returns the string "Hello, " concatenated with the name.
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+# Define a method starts_with_consonant?(s) that takes a string and
+# returns true if it starts with a consonant and false otherwise.
+# (For our purposes, a consonant is any letter other than A, E, I, O, U.)
+# NOTE: be sure it works for both upper and lower case and for non-letters!
+def starts_with_consonant?(s)
+  return false if s.empty?
+  return false if /^[[:alpha:]]/.match(s).nil?
+  /^[^aeiou]/i.match(s)
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+# Define a method binary_multiple_of_4?(s) that takes a string and
+# returns true if the string represents a binary number that is a multiple of 4.
+# NOTE: be sure it returns false if the string is not a valid binary number!
+def binary_multiple_of_4?(s)
+  return false if s.empty?
+  return false if s =~ /[^01]/
+  (s.to_i(2) % 4).zero?
 end
 
 # Part 3
