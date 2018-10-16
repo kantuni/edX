3fda0c9287aff88e1ecfc895f330c8190489f6fe
kantuni <henrikh.kantuni@gmail.com>
2018-10-16 13:47:27 -0400

    Put CS169.1x into a separate repo

--- BerkeleyX/CS169.1x/hw-sinatra-saas-hangperson/Part 0/app.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra'
-
-class App < Sinatra::Base
-  get '/' do
-    'Goodbye, world!'
-  end
-end
--- BerkeleyX/CS169.1x/hw-sinatra-saas-hangperson/Part 1/features/support/webmock.rb
+++
@@ -1,5 +0,0 @@
-require 'webmock/cucumber'
-
-Before do
-  stub_request(:post, "http://watchout4snakes.com/wo4snakes/Random/RandomWord").to_return(:status => 200, :headers => {}, :body => "testword")
-end
